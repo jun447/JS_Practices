f900ec35d5a67d4e55b558fa4d709f1a36278861
jun447 <junaidmumtaz16@gmail.com>
2023-11-05 02:49:33 +0500

    scope added

---
+++ basic_Functions/function_1.js
@@ -0,0 +1,47 @@
+// function in js
+// function to multiply two numbers
+// function multiply(a, b) {
+//     return a * b;
+// }
+// function to add two numbers
+// console.log(multiply(2, 3));
+
+//function with default paameters
+// function def_multiply(a, b = 4) {
+//     return a * b;
+// }
+// console.log(def_multiply(2)); // 2
+
+// using rest parameters
+// function addToCart(...items) {
+//     return items;
+// }
+// console.log(addToCart('shirt', 'pant', 'shoes')); // [ 'shirt', 'pant', 'shoes' ]
+
+// function to handle objects
+
+// const cart = {
+//     name: 'shirt',
+//     price: 100,
+// }
+//
+// function printCart(obj) {
+//     return `The ${obj.name} costs ${obj.price}`;
+// }
+// console.log(printCart(cart)); // The shirt costs 100
+
+
+if (true) {
+ let a = 10;
+ const b = 20;
+ var c = 30;
+}
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// The scope of var is global
+
+
+
+
