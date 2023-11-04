9c3d897d1956e11dcf2dfdc4e8bb688b4ce23fb7
jun447 <junaidmumtaz16@gmail.com>
2023-11-04 17:43:47 +0500

    Added math with random

---
+++ Basic_1/math.js
@@ -0,0 +1,13 @@
+// console.log(Math.PI)
+
+// console.log(Math.round(4.7))
+// will give random value between 0 and 5
+// a=Math.random()*5
+// console.log(a + 1)
+// console.log(a )
+// console.log(Math.round(a) )
+
+const min = 1;
+const max =10
+const rand = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(rand)
