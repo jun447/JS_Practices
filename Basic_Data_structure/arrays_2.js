49036ae197ea4ca072676b9955b3e68d31669b52
jun447 <junaidmumtaz16@gmail.com>
2023-11-04 19:21:34 +0500

    Added arrays operations

---
+++ Basic_Data_structure/arrays_2.js
@@ -0,0 +1,27 @@
+const mrvl_heros = ["thor",'ironman',"spiderman"]
+
+const dc = ["batman","superman","flash"]
+
+// mrvl_heros.push(dc)
+//
+// console.log(mrvl_heros)
+// console.log(dc)
+
+// const mrvl_dc = mrvl_heros.concat(dc)
+// console.log(mrvl_dc)
+
+// spread operator
+// const mrvl_dc = [...mrvl_heros,...dc]
+// console.log(mrvl_dc)
+
+// flat method example
+// const mrvl_dc = [mrvl_heros,dc]
+// console.log(mrvl_dc)
+// console.log(mrvl_dc.flat())
+
+// console.log(Array.isArray(mrvl_heros))
+// console.log(Array.from("Ghori"))
+// console.log(Array.from({name:"Ghori"}))
+
+//give example of Array.of()
+console.log(Array.of(1,2,3,4,5))
