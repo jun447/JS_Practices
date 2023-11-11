7bb078781c5d9a1f6ce6638acac0ce0d77242497
jun447 <junaidmumtaz16@gmail.com>
2023-11-12 01:19:37 +0500

    Random Background Color changer

---
+++ dom_projects/6-unlimitedColors/index.js
@@ -0,0 +1,19 @@
+
+const btn_strt = document.querySelector('#start')
+const btn_stop = document.querySelector('#stop')
+
+let intervalId = null
+btn_strt.addEventListener('click',function () {
+    intervalId = setInterval(function () {
+        // random color generator
+        const randomColor = Math.floor(Math.random()*16777215).toString(16)
+        document.body.style.backgroundColor = '#'+randomColor
+        console.log('start Clicked')
+    },1000)
+})
+
+btn_stop.addEventListener('click',function () {
+    clearInterval(intervalId)
+    console.log('stop Clicked')
+})
+
