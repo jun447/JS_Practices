b6bb971366640a459df2b14101966c88bd480d1a
jun447 <junaidmumtaz16@gmail.com>
2023-11-12 01:49:28 +0500

    KeyBoard Tester Done

--- dom_projects/5-keyboard/index.js
+++ dom_projects/5-keyboard/index.js
@@ -1,1 +1,11 @@
-console.log('Project 5');
+const insrt = document.querySelector('#insert')
+
+window.addEventListener('keydown',function (e){
+    e.preventDefault()
+    if (e.key === ' '){
+        insrt.innerHTML='Space'
+    }else {
+        insrt.innerHTML=e.key
+    }
+
+})
